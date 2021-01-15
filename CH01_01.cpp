fa3f26bc11ea5dcc20ee59dd099bd8525b8b465b
YWB <2072353842@qq.com>
2021-01-15 09:56:41 +0800

    233

---
+++ CH01_01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()//括号里为系统传递给函数的参数，调用main函数时，无参数但需返回整数 
+{
+	cout<<"我的第一个程序"<<endl;//endl作用为控制换行  “”里包含的为一个字符串，这行表示打印字符串 
+	return  0://表示返回一个值给操作系统，如果返回值为零，就表示停止运行程序并且将控制权返回给操作系统  
+}  
+
